6d48c735cce057a71b653289befbc946de4cf64e
wangjun <13410083463@163.com>
2019-04-02 23:10:59 +0800

    fdsfdsf

--- src/script/resetpass.js
+++ src/script/resetpass.js
@@ -7,6 +7,11 @@ $(function(){
         var Pass = $("#pass").val();
         var Newpass = $("#newPass").val();
         var Confirmpass = $("#confirmPass").val();
+        console.log(Newpass,Confirmpass)
+        var data = {
+            original_password:Pass,
+            new_password:Newpass
+        }
         if (Pass == ""){
             pass = "- 请输入原密码\n\n"
         }
@@ -20,8 +25,25 @@ $(function(){
             alert(pass + newPass + confirmPass)
         } else if (Newpass != Confirmpass){
             alert("- 新密码不一致，请重新输入")
+        }else{
+            $.ajax({
+                url:"http://declare.dagaimao.cn/web/index.php?r=users/deal-password",
+                type:"post",
+                data:data,
+                xhrFields:{
+                    withCredentials: true//设置显式指定浏览器发送Cookie，跨域时默认不使用
+                },
+                crossDomain: true,
+                dataType:"json",
+                success:function(res){
+                    console.log(res)
+                },
+                error:function(){
+                    console.log("error")
+                }
+            })
         }
-      
+        
       
         return false;
 
